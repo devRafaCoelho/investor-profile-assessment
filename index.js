3e1ca444e7f0f810c07fa8401f59b488458483e5
devRafaCoelho <108550900+devRafaCoelho@users.noreply.github.com>
2022-08-21 15:12:47 -0300

    new html and css structure

---
+++ index.js
@@ -0,0 +1,89 @@
+const personal = [
+    {
+        label: '',
+        inputPlaceholder: ''
+    },
+    {
+        label: '',
+        inputPlaceholder: ''
+    }, {
+        label: '',
+        inputPlaceholder: ''
+    }
+]
+
+const questions = [
+    {
+        pQuestion: '',
+        inputLabel1: '',
+        inputLabe21: '',
+        inputLabel3: '',
+        inputLabel4: '',
+        inputLabel5: ''
+    },
+    {
+        pQuestion: '',
+        inputLabel1: '',
+        inputLabe21: '',
+        inputLabel3: '',
+        inputLabel4: '',
+        inputLabel5: ''
+    },
+    {
+        pQuestion: '',
+        inputLabel1: '',
+        inputLabe21: '',
+        inputLabel3: '',
+        inputLabel4: '',
+        inputLabel5: ''
+    },
+    {
+        pQuestion: '',
+        inputLabel1: '',
+        inputLabe21: '',
+        inputLabel3: '',
+        inputLabel4: '',
+        inputLabel5: ''
+    },
+    {
+        pQuestion: '',
+        inputLabel1: '',
+        inputLabe21: '',
+        inputLabel3: '',
+        inputLabel4: '',
+        inputLabel5: ''
+    },
+    {
+        pQuestion: '',
+        inputLabel1: '',
+        inputLabe21: '',
+        inputLabel3: '',
+        inputLabel4: '',
+        inputLabel5: ''
+    },
+    {
+        pQuestion: '',
+        inputLabel1: '',
+        inputLabe21: '',
+        inputLabel3: '',
+        inputLabel4: '',
+        inputLabel5: ''
+    },
+    {
+        pQuestion: '',
+        inputLabel1: '',
+        inputLabe21: '',
+        inputLabel3: '',
+        inputLabel4: '',
+        inputLabel5: ''
+    },
+    {
+        pQuestion: '',
+        inputLabel1: '',
+        inputLabe21: '',
+        inputLabel3: '',
+        inputLabel4: '',
+        inputLabel5: ''
+    }
+]
+
