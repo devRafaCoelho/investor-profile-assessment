06abec828e9c17258b306b38f6b796f7acb37085
devRafaCoelho <108550900+devRafaCoelho@users.noreply.github.com>
2022-09-07 11:44:05 -0300

    created link betwee cards and content

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const cards = document.querySelectorAll('.card');
 const cardsContainer = document.querySelector('.questions-cards');
+const content = document.querySelector('.questions-content');
+const position = document.querySelectorAll('.position');
+const image = document.querySelector('.questions-content img');
 
-cards.forEach((element) => {
+cards.forEach((element, index) => {
     element.addEventListener('click', (event) => {
         cardsContainer.classList.add('hidden');
+        position[index].classList.remove('hidden');
+        content.classList.remove('hidden');
+        image.classList.remove('hidden');
     });
 })
 
