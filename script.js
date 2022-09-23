669dfb238ed9d0e39e5ef2a64ab3e95ac5b8cd79
devRafaCoelho <108550900+devRafaCoelho@users.noreply.github.com>
2022-09-23 11:54:12 -0300

    code review and media queries

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
 const cards = document.querySelectorAll('.card');
 const cardsContainer = document.querySelector('.questions-cards');
-const content = document.querySelector('.questions-content');
 const position = document.querySelectorAll('.position');
 const image = document.querySelector('.questions-content img');
 let lastIndex = [];
 
 cards.forEach((element, index) => {
-    element.addEventListener('click', (event) => {
-        cardsContainer.classList.add('hidden');
-        position[index].classList.remove('hidden');
-        content.classList.remove('hidden');
-        image.classList.remove('hidden');
-
-        lastIndex.push(index);
-
-        if (lastIndex.length > 1 && index === lastIndex[0]) {
-            lastIndex.shift();
-        } else if (lastIndex.length > 1 && index !== lastIndex[0]) {
-            position[lastIndex[0]].classList.add('hidden');
-            lastIndex.shift();
-        }
+    element.addEventListener('click', () => {
+        cardsContainer.style.display = 'none';
+        position[index].style.display = 'flex';
+        image.style.display = 'flex';
+        lastIndex = index;
     });
 });
 
 image.addEventListener('click', () => {
-    cardsContainer.classList.remove('hidden');
-    content.classList.add('hidden');
-    image.classList.add('hidden');
+    position[lastIndex].style.display = 'none';
+    cardsContainer.style.display = 'flex';
+    image.style.display = 'none';
 });
